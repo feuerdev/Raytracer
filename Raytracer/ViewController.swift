bf7ec6114dcd850f4fa5dbb35014a1ecf11f7ce5
Jannik Feuerhahn <jannik@feuer.dev>
2021-03-12 14:19:46 +0800

    updated to new raytracer api

--- Raytracer/ViewController.swift
+++ Raytracer/ViewController.swift
@@ -15,41 +15,50 @@ class ViewController: UIViewController {
         
         var spheres = [Sphere]()
         for _ in 1...30 {
-            let sphere = Sphere(center: Vector3(Float.random(in: -30...30), Float.random(in: -30...30), Float.random(in: 0...100)), radius: Float.random(in: 0.3...12), color: UInt32(a:255, r:Int.random(in: 0...255), g:Int.random(in: 0...255), b:Int.random(in: 0...255)))
+            let sphere = Sphere(
+                center: Vector3(Float.random(in: -30...30), Float.random(in: -30...30), Float.random(in: 0...100)),
+                radius: Float.random(in: 0.3...12),
+                color: RGBColor(Int.random(in: 0...255), Int.random(in: 0...255), Int.random(in: 0...255)),
+                specular: Int.random(in: -1...1000),
+                reflectivity: Float.random(in: 0...0.5))
             spheres.append(sphere)
         }
 
         let debugSpheres = [
-            Sphere(center:Vector3(0, -1, 3), radius: 1, color: 0xFFFF0000),
-            Sphere(center: Vector3(2, 0, 4), radius: 1, color: 0xFF0000FF),
-            Sphere(center: Vector3(-2, 0, 4), radius: 1, color: 0xFF00FF00),
-            Sphere(center: Vector3(0, -5001, 0), radius: 5000, color: 0xFFFFFF00)
+            Sphere(center:Vector3(0, -1, 3), radius: 1, color: .white, specular: 1000, reflectivity: 0.2), //Red
+            Sphere(center: Vector3(2, 0, 4), radius: 1, color: .blue, specular: 500, reflectivity: 0.3), //Blue
+            Sphere(center: Vector3(-2, 0, 4), radius: 1, color: .green, specular: 10, reflectivity: 0.4), //Green
+            Sphere(center: Vector3(0, -5005, 0), radius: 5000, color: .init(20, 20, 20), specular: 1000, reflectivity: 0) //Yellow
         ]
 
         let lights = [
-            Light(type: .ambient, intensity: 0.2),
-            Light(type: .point, intensity: 0.6, position: .init(2, 1, 0)),
-            Light(type: .directional, intensity: 0.2, direction: .init(1, 4, 4))
+//            Light(type: .ambient, intensity: 0.4),
+            Light(type: .point, intensity: 1, position: .init(400, 200, -10)),
+//            Light(type: .directional, intensity: 0.2, direction: .init(1, 4, 4))
         ]
 
-        let scene = Scene(spheres: spheres,
-                          lights: lights,
-                          cameraPosition: Vector3(0, 0, 0),
-                          viewportSize: 1,
-                          projectionPlane: 1)
+        let scene = Scene(spheres: debugSpheres,
+                          lights: lights)
 
+        print(UIScreen.main.nativeBounds.width)
+        let quality:CGFloat = 0.1
         let raytracer = Raytracer(
-            width: 100,//Int(view.frame.width),
-            height: 100,//Int(view.frame.height),
-            scene: scene)
+            width: Int(UIScreen.main.nativeBounds.width*quality),
+            height: Int(UIScreen.main.nativeBounds.height*quality),
+            scene: scene,
+            background: .black,
+            rDepth: 2)
 
-        Timer.scheduledTimer(withTimeInterval: 1/24, repeats: true) { (timer) in
+        Timer.scheduledTimer(withTimeInterval: 1/24, repeats: false) { (timer) in
             raytracer.scene.cameraPosition = Vector3(
                 raytracer.scene.cameraPosition.x,
                 raytracer.scene.cameraPosition.y,
                 raytracer.scene.cameraPosition.z+1)
             self.view.layer.contents = raytracer.draw()
         }
+        
     }
+}
+
 }
 
